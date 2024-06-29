1d2d06c6899004356233e35364d0e9e89c5f025d
Anupamkush9 <anupam.kush9@gmail.com>
2024-06-29 23:38:50 +0530

    image-and-file-upload-drf-api-implemented

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+from pyexpat import model
+from rest_framework import serializers
+from api.models import Profile
+from django.core.exceptions import ValidationError
+
+def validate_file_size(value):
+  filesize = value.size # returns the file size in byte
+
+  if filesize > 1048576:  # 1MB limit
+      raise ValidationError(f"The maximum file size that can be uploaded is 1048576 bytes but your file size is {filesize} bytes")
+  return value
+
+class ProfileSerializer(serializers.ModelSerializer):
+  rdoc = serializers.FileField(validators=[validate_file_size])
+
+  class Meta:
+    model=Profile
+    fields = ['id', 'name', 'email', 'dob', 'state', 'gender', 'location', 'pimage', 'rdoc']
